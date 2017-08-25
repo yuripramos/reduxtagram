bb7e59c5c687b9487011ffdf2cfe52100f85b9e4
Yuri Ramos <yuri.pramos@gmail.com>
2017-08-25 10:16:07 -0300

    adding comment component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,12 +1,33 @@
-import React from  'react';
+import React, { Component } from  'react';
 
 
-const Comments = (props) => {
-  return(
-    <div className="comment">
-      I'm the comments
-    </div>
-  );
+class Comments extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  renderComment(comment, i) {
+    console.log(comment);
+  }
+
+  render() {
+    const firstRender = this.props.postComments.map((comment, i) => {
+      return (
+        <div className="comment" key={i}>
+          <p>
+            <strong> {comment.user} </strong>
+            {comment.text}
+            <button className="remove-comment">&times;</button>
+          </p>
+        </div>
+      );
+    });
+    return(
+      <div className="comment">
+        {firstRender}
+      </div>
+    );
+  }
 };
 
 export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -3,16 +3,19 @@ import Photo from './Photo';
 import Comments from './Comments';
 
 const Single = (props) =>{
+  const { postId } = props.params;
+
   const i = props.posts.findIndex((post) => {
     return (
-      post.code === props.params.postId
+      post.code === postId
     )
   });
   const post = props.posts[i];
+  const postComments = props.comments[postId] || [];
   return (
     <div className="single-photo">
       <Photo i={i} keyId={i} post={post} {...props} />
-      <Comments />
+      <Comments postComments={postComments}/>
     </div>
   )
 };
